429379c033acdacd7a9e693e9c9215520644a63f
KID421 <suchild7@gmail.com>
2020-03-22 12:51:58 +0800

    完成前往指定元素效果

--- goToElement.js
+++ goToElement.js
@@ -1,9 +1,16 @@
 $("*").each(function () {
     $(this).click(function () { 
+        // 目標屬性
         var target = $(this).attr("data-goto-target");
-        var duration = $(this).attr("data-goto-duration");
 
-        console.log("目標元素：" + target);
-        console.log("時間：" + duration);
+        // 如果 目標屬性 不等於 未定義資料
+        if (target != undefined) {
+            // 時間屬性
+            var duration = $(this).attr("data-goto-duration");
+            // 上方位置 = 目標屬性.位移.上方
+            var top = $(target).offset().top;
+            // 動畫({捲動上方：上方位置}，時間)
+            $("html, body").animate({scrollTop: top}, parseInt(duration));
+        }
     });
 });
