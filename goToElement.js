56f5fdbfdea3cb4c057912b7e2a9694dae629e13
KID421 <suchild7@gmail.com>
2020-03-22 12:38:19 +0800

    使用 jQuery 取得點擊屬性資料

--- goToElement.js
+++ goToElement.js
@@ -1,1 +1,9 @@
-$("#box1").fadeOut();
+$("*").each(function () {
+    $(this).click(function () { 
+        var target = $(this).attr("data-goto-target");
+        var duration = $(this).attr("data-goto-duration");
+
+        console.log("目標元素：" + target);
+        console.log("時間：" + duration);
+    });
+});
